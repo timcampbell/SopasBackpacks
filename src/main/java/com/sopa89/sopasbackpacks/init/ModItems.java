e79e150c7753030276cb47fe80ec240e1717c08a
timcampbell <tim.campbell127@gmail.com>
2014-07-15 21:19:19 -0400

    Added Leather Strip Item

--- src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
+++ src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.sopa89.sopasbackpacks.init;
 
 import com.sopa89.sopasbackpacks.item.ItemBackpack;
+import com.sopa89.sopasbackpacks.item.ItemStripLeather;
 import com.sopa89.sopasbackpacks.item.ItemYarnSpool;
 import com.sopa89.sopasbackpacks.item.ItemsSB;
 
@@ -10,10 +11,12 @@ public class ModItems
 {
 	public static ItemsSB backpack=new ItemBackpack();
 	public static ItemsSB yarnSpool=new ItemYarnSpool();
+	public static ItemsSB stripLeather=new ItemStripLeather();
 	
 	public static void init()
 	{
-		GameRegistry.registerItem(backpack, "Backpack");
-		GameRegistry.registerItem(yarnSpool, "YarnSpool");
+		GameRegistry.registerItem(backpack, "backpack");
+		GameRegistry.registerItem(yarnSpool, "yarnSpool");
+		GameRegistry.registerItem(stripLeather, "stripLeather");
 	}
 }
---
+++ src/main/java/com/sopa89/sopasbackpacks/item/ItemStripLeather.java
@@ -0,0 +1,10 @@
+package com.sopa89.sopasbackpacks.item;
+
+public class ItemStripLeather extends ItemsSB
+{
+	public ItemStripLeather()
+	{
+		super();
+		this.setUnlocalizedName("stripLeather");
+	}
+}
