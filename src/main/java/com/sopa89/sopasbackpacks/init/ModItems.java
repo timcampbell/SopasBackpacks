31f6c30e290af84c5ce9841e63101335c4b2b5f8
timcampbell <tim.campbell127@gmail.com>
2014-07-15 21:08:37 -0400

    Added in Spool of Yarn Item

--- src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
+++ src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.sopa89.sopasbackpacks.init;
 
 import com.sopa89.sopasbackpacks.item.ItemBackpack;
+import com.sopa89.sopasbackpacks.item.ItemYarnSpool;
 import com.sopa89.sopasbackpacks.item.ItemsSB;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -8,9 +9,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModItems 
 {
 	public static ItemsSB backpack=new ItemBackpack();
+	public static ItemsSB yarnSpool=new ItemYarnSpool();
 	
 	public static void init()
 	{
 		GameRegistry.registerItem(backpack, "Backpack");
+		GameRegistry.registerItem(yarnSpool, "YarnSpool");
 	}
 }
---
+++ src/main/java/com/sopa89/sopasbackpacks/item/ItemYarnSpool.java
@@ -0,0 +1,10 @@
+package com.sopa89.sopasbackpacks.item;
+
+public class ItemYarnSpool extends ItemsSB
+{
+	public ItemYarnSpool()
+	{
+		super();
+		this.setUnlocalizedName("yarnSpool");
+	}
+}
