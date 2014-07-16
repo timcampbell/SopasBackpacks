3c5797cef3ad84fb0bb254d02b9d1388b5ef460b
timcampbell <tim.campbell127@gmail.com>
2014-07-15 21:31:08 -0400

    Added Backpack Bag Item

--- src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
+++ src/main/java/com/sopa89/sopasbackpacks/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.sopa89.sopasbackpacks.init;
 
 import com.sopa89.sopasbackpacks.item.ItemBackpack;
+import com.sopa89.sopasbackpacks.item.ItemBackpackBag;
 import com.sopa89.sopasbackpacks.item.ItemStripLeather;
 import com.sopa89.sopasbackpacks.item.ItemYarnSpool;
 import com.sopa89.sopasbackpacks.item.ItemsSB;
@@ -12,11 +13,13 @@ public class ModItems
 	public static ItemsSB backpack=new ItemBackpack();
 	public static ItemsSB yarnSpool=new ItemYarnSpool();
 	public static ItemsSB stripLeather=new ItemStripLeather();
+	public static ItemsSB backpackBag=new ItemBackpackBag();
 	
 	public static void init()
 	{
 		GameRegistry.registerItem(backpack, "backpack");
 		GameRegistry.registerItem(yarnSpool, "yarnSpool");
 		GameRegistry.registerItem(stripLeather, "stripLeather");
+		GameRegistry.registerItem(backpackBag, "backpackBag");
 	}
 }
---
+++ src/main/java/com/sopa89/sopasbackpacks/item/ItemBackpackBag.java
@@ -0,0 +1,10 @@
+package com.sopa89.sopasbackpacks.item;
+
+public class ItemBackpackBag extends ItemsSB
+{
+	public ItemBackpackBag()
+	{
+		super();
+		this.setUnlocalizedName("backpackBag");
+	}
+}
